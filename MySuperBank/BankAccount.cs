8d8f5adb3768441e4811cbef86cd23a6f93e0ede
Zeusmist <daviderinayo@gmail.com>
2021-01-11 22:15:02 +0100

    initial commit

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySuperBank
+{
+    class BankAccount
+    {
+        /* The Properties */
+        public string Number { get; }
+        public string Owner { get; set; }
+
+        private static int accountNumberSeed = 1234567890;
+        private List<Transaction> allTransactions = new List<Transaction>();
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var transaction in allTransactions)
+                {
+                    balance = balance + transaction.Amount;
+                }
+                return balance;
+            }
+        }
+
+        /* The Constructor */
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Number = accountNumberSeed.ToString();
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            accountNumberSeed++;
+        }
+
+        /* The Methods */
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawl = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawl);
+            Console.WriteLine($"You made a withdrawl, your current balance is {Balance}");
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            /* HEADER */
+            report.AppendLine("Date\t\tAmount\t\tNote");
+            report.AppendLine("--------------------------------------------------------------");
+
+            foreach (var transaction in allTransactions)
+            {
+                /* ROWS */
+                report.AppendLine($"{transaction.Date.ToShortDateString()}\t{transaction.Amount}\t\t{transaction.Notes}");
+            }
+
+            return report.ToString();
+        }
+
+
+
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 10000);
+            Console.WriteLine($"Accout {account.Number} was created for {account.Owner} with {account.Balance}.\n\n");
+
+            account.MakeWithdrawal(14.750m, DateTime.Now, "Hammock");
+            account.MakeWithdrawal(500, DateTime.Now, "PS5");
+
+            Console.WriteLine("\n\nWould you like to see a list of all your transactions?\nEnter 0: No\tor\tEnter 1: Yes");
+            var userInput = Convert.ToInt32(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 0:
+                    Console.WriteLine("Thank you for banking with us");
+                    break;
+                case 1:
+                    Console.WriteLine("\n\n"+account.GetAccountHistory());
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection");
+                    break;
+            }
+
+
+
+
+
+            /* TESTS: uncomment all code below once to run tests*/
+
+            ///* Test that the initial balances must be positive */
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch(ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            ///* Test for a negative balance */
+            //try
+            //{
+            //    account.MakeWithdrawal(75000, DateTime.Now, "Attempt to overdraw");
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+        }
+    }
+}
